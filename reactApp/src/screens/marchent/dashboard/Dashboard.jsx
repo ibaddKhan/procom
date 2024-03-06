69cd70f38d9ac9c3b10852fb7472eaae2118bd11
hashimrzaa <hashimterminator20@gmail.com>
2024-03-06 21:57:46 +0500

    update

--- reactApp/src/screens/marchent/dashboard/Dashboard.jsx
+++ reactApp/src/screens/marchent/dashboard/Dashboard.jsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import PieChart from '../../../components/marchents/PieChart'
-=======
-import React from 'react'
-import { Box, Typography } from '@mui/material'
-import PieChart from '../../../components/marchents/PieChart'
-
->>>>>>> 0826ce13e95818438a4a7666cbc015b823a57dbe
+import PieChart from "../../../components/marchents/PieChart";
 
 const Dashboard = () => {
   return (
@@ -15,15 +8,7 @@ const Dashboard = () => {
       <Box>
         <Typography variant="h5"> Dashboard </Typography>
 
-<<<<<<< HEAD
-        <PieChart/>
-=======
-        <Typography variant='h5' sx={{marginBottom:"50px"}}> Dashboard </Typography>
-
         <PieChart />
-
-
->>>>>>> 0826ce13e95818438a4a7666cbc015b823a57dbe
       </Box>
     </>
   );
