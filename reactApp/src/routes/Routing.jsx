67e8e82ab47a487935c755133e2b86b8b24dc62b
hashimrzaa <hashimterminator20@gmail.com>
2024-03-06 20:04:57 +0500

    update

--- reactApp/src/routes/Routing.jsx
+++ reactApp/src/routes/Routing.jsx
@@ -4,6 +4,9 @@ import Login from "../screens/Auth/Login/Login";
 import Register from "../screens/Auth/Register/Register";
 import Marchent from "../screens/marchent/Marchent";
 import Customer from "../screens/customer/Customer";
+import Payment from "../screens/customer/payments/Payment";
+import InstantPayment from "../screens/customer/InstantPayment/InstantPayment";
+import Qrscan from "../screens/customer/Qrscan/Qrscan";
 const Routing = () => {
   return (
     <BrowserRouter>
@@ -11,8 +14,12 @@ const Routing = () => {
         <Route path="/" element={<>Home</>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/marchent" element={<Marchent />} />
-        <Route path="/customer" element={<Customer />} />
+        <Route path="/marchent" element={<Customer />}>
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/instantpayment" element={<InstantPayment />} />
+          <Route path="/qrscan" element={<Qrscan />} />
+        </Route>
+        <Route path="/customer" element={<Marchent />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ reactApp/src/screens/customer/Qrscan/Qrscan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Qrscan = () => {
+  return (
+    <div>Qrscan</div>
+  )
+}
+
+export default Qrscan
