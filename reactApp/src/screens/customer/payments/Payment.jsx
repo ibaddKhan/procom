f0ef4a55c937578659948e8391bf153cdb20a4cb
hashimrzaa <hashimterminator20@gmail.com>
2024-03-07 13:04:13 +0500

    update

--- reactApp/src/screens/customer/payments/Payment.jsx
+++ reactApp/src/screens/customer/payments/Payment.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
-import PaymentCard from '../../../components/customers/paymentCard'
-import PaymentList from '../../../components/customers/paymentList'
-import TableComponent from '../../../components/Table'
+import React from "react";
+import PaymentCard from "../../../components/customers/paymentCard";
+import PaymentList from "../../../components/customers/paymentList";
+import TableComponent from "../../../components/Table";
 
 const Payment = () => {
   return (
     <>
-      <h1 className='font-weight-bold'>Payment</h1>
+      <h1 className="font-weight-bold">Payment</h1>
       <div className="d-flex flex-wrap mb-3">
         <div className="p-2">
-          <PaymentCard buttonValColor="#42a5f5
-" recordsVal="199" data="Total Pending Records" color="#e0f7fa" />
+          <PaymentCard
+            buttonValColor="#42a5f5
+"
+            recordsVal="199"
+            data="Total Pending Records"
+            color="#e0f7fa"
+          />
         </div>
         <div className="p-2 ">
-          <PaymentCard buttonValColor="#009688
-" recordsVal="199" data="Total Paid Records" color="#e0f2f1
+          <PaymentCard
+            buttonValColor="#009688
+"
+            recordsVal="199"
+            data="Total Paid Records"
+            color="#e0f2f1
 
-" />
+"
+          />
         </div>
         <div className="p-2">
-          <PaymentCard buttonValColor="#e57373
+          <PaymentCard
+            buttonValColor="#e57373
 
-" recordsVal="199" data="Total Rejected Records" color="#ffebee
-" />
+"
+            recordsVal="199"
+            data="Total Rejected Records"
+            color="#ffebee
+"
+          />
         </div>
       </div>
       <PaymentList />
     </>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
